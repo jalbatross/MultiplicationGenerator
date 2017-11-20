2d4d9ace89c4712a618abdbe3f966dc7db1c7ad5
jalbatross <ih8u2o@gmail.com>
2017-11-19 21:38:53 -0800

    Counting number of combos of number multiplications

--- generator.py
+++ generator.py
@@ -1,33 +1,65 @@
 #generator.py
 import random
+import operator as op
 
-num_digits_a = 4;
-num_digits_b = 3;
+def ncr(n, r):
+    r = min(r, n-r)
+    if r == 0: return 1
+    numer = reduce(op.mul, xrange(n, n-r, -1))
+    denom = reduce(op.mul, xrange(1, r+1))
+    return numer//denom
 
-def generateNumbers():
+
+def generateNumbers(numDigitsA, numDigitsB):
 	random.seed()
 	aDigits = [];
 	bDigits = [];
 
-	for i in range (0, num_digits_a):
+	for i in range (0, numDigitsA):
 		aDigits.append(random.randint(1, 9)) if i == 0 else aDigits.append(random.randint(0,9))
 	
-	for j in range (0, num_digits_b):
+	for j in range (0, numDigitsB):
 		bDigits.append(random.randint(1, 9)) if j == 9 else bDigits.append(random.randint(0,9))
 
+	#How does this work?
 	a = reduce(lambda x, y: str(x) + str(y), aDigits)
 	b = reduce(lambda x, y: str(x) + str(y), bDigits)
 
 	ret = [int(a), int(b)]
+
 	return ret
 
 def displayProduct(numbers):
 	print numbers[0] * numbers[1]
 
+def identifyCombinations(numbers):
+	digitsA = list(map(int, str(numbers[0])) )
+	digitsB = list(map(int, str(numbers[1])) )
+
+	booleans = [False] * 9;
+
+	for i in digitsA:
+		booleans[i - 1] = True
+
+	for j in digitsB:
+		booleans[j - 1] = True
+
+	#Count the number of different digits for each
+	numDigits = 0
+	for k in booleans:
+		if k == True:
+			numDigits += 1
+
+	return ncr(numDigits + 1, 2)
 
 def main():
-	numbers = generateNumbers()
-	print numbers
-	displayProduct(numbers)
+	numDigitsA = 4
+	numDigitsB = 3;
+	numbers = generateNumbers(numDigitsA, numDigitsB)
+	combos = identifyCombinations(numbers)
 
+	print numbers
+	print "num combos: ", combos
+	displayProduct(numbers)
+	print "end"
 main()
